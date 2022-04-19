8d73b0d1a9bd2a48bd1630bae7fd9fc5185b6494
Zoruark8 <31805544+Zoruark8@users.noreply.github.com>
2022-04-19 18:14:41 -0500

    Added logic to inRow and inColumn methods in SudokuDriver

--- src/SudokuMain/SudokuDriver.java
+++ src/SudokuMain/SudokuDriver.java
@@ -1,15 +1,31 @@
 package SudokuMain;
 public class SudokuDriver {
 
+    private static final int BOARD_SIZE = 9;
+
     public static void main(String args[]) {
 
     }
 
     public static boolean inRow(int value, int row, int[][] board) {
+
+        for (int i = 0; i < BOARD_SIZE; i++) {
+
+            if (value == board[row][i]) {
+                return true;
+            }
+        }
         return false;
     }
 
     public static boolean inColumn(int value, int column, int[][] board) {
+
+        for (int i = 0; i < BOARD_SIZE; i++) {
+
+            if (value == board[i][column]) {
+                return true;
+            }
+        }
         return false;
     }
 
